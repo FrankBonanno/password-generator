cd870223788a6673cb7ceac1d14e09813b700d5a
FrankBonanno <fb0023@mix.wvu.edu>
2022-10-25 14:03:20 -0400

    added char selection buttons

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const characters = [
+const letters = [
 	"A",
 	"B",
 	"C",
@@ -51,16 +51,8 @@ const characters = [
 	"x",
 	"y",
 	"z",
-	"0",
-	"1",
-	"2",
-	"3",
-	"4",
-	"5",
-	"6",
-	"7",
-	"8",
-	"9",
+];
+const symbols = [
 	"~",
 	"`",
 	"!",
@@ -88,24 +80,48 @@ const characters = [
 	"<",
 	">",
 	"?",
+	".",
 	"/",
 ];
+const numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
+let hasLetters = true;
+let hasNumbers = true;
+let hasSymbols = true;
 
 let passwordOneEl = document.getElementById("password-one");
 let passwordTwoEl = document.getElementById("password-two");
 
-function generateRandomChar() {
+let lettersEl = document.getElementById("letters-el");
+let numbersEl = document.getElementById("numbers-el");
+let symbolsEl = document.getElementById("symbols-el");
+
+let copyEl = document.getElementById("copy-el");
+
+function getRandomChar() {
+	let characters = [];
+
+	if (hasLetters) {
+		characters.push(...letters);
+	}
+	if (hasNumbers) {
+		characters.push(...numbers);
+	}
+	if (hasSymbols) {
+		characters.push(...symbols);
+	}
+
 	let randIndex = Math.floor(Math.random() * characters.length);
 
 	return characters[randIndex];
 }
 
-function genPasswords() {
+function renderPasswords() {
 	let passwordOne = "";
 	let passwordTwo = "";
 	for (let i = 0; i < 15; i++) {
-		passwordOne += generateRandomChar();
-		passwordTwo += generateRandomChar();
+		passwordOne += getRandomChar();
+		passwordTwo += getRandomChar();
 	}
 
 	passwordOneEl.textContent = passwordOne;
@@ -114,7 +130,49 @@ function genPasswords() {
 
 function clickToCopy(id) {
 	let copyText = document.getElementById(`password-${id}`);
+	if (copyText.textContent === "") return;
 
 	navigator.clipboard.writeText(copyText.textContent);
-	alert("Copied: " + copyText.textContent);
+	copyEl.textContent =
+		"Copied Password To Clipboard: " + copyText.textContent;
+}
+
+const buttonDark = `
+	background-color: var(--clr-blue-light);
+	color: var(--clr-blue-dark)
+`;
+
+const buttonLight = `
+	background-color: var(--btn-green);
+	color: var(--txt-white);
+`;
+
+function setChars(id) {
+	if (id === "letters") {
+		hasLetters = !hasLetters;
+	} else if (id === "numbers") {
+		hasNumbers = !hasNumbers;
+	} else if (id === "symbols") {
+		hasSymbols = !hasSymbols;
+	}
+
+	if (!hasLetters && !hasNumbers && !hasSymbols) {
+		hasLetters = true;
+	}
+
+	if (hasLetters) {
+		lettersEl.style.cssText = buttonLight;
+	} else {
+		lettersEl.style.cssText = buttonDark;
+	}
+	if (hasNumbers) {
+		numbersEl.style.cssText = buttonLight;
+	} else {
+		numbersEl.style.cssText = buttonDark;
+	}
+	if (hasSymbols) {
+		symbolsEl.style.cssText = buttonLight;
+	} else {
+		symbolsEl.style.cssText = buttonDark;
+	}
 }
